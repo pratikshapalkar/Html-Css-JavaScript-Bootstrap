d6f4def36449fd10d6d33b555970542495c4a972
Prati-1234 <pratikshapalkar141@gmail.com>
2022-01-07 18:53:43 +0530

    Javascript programs on ifelse,switch case etc.

---
+++ index.js
@@ -0,0 +1,83 @@
+
+//1 .ways to print in javascript
+console.log("Hello world");
+//alert("me");
+document.write("This is document.write")
+//2.javascript console api
+console.log("this is log" ,4+6, "another log");
+console.error("this is error");
+console.warn("this is warning")
+
+//3.javascript variables
+var num1=23;
+var num2=56;
+console.log(num1+num2);
+
+//4.data types in javascript
+//string..
+var str1="this is a string";
+var str2='string';
+//numbers..
+var num1=455;
+var num2=56.98;
+//objects..
+var marks={
+    pratiksha:78.98,
+    amit:99.99,
+    priya:80
+}
+console.log(str1);
+console.log(marks);
+//booleans
+var a=true;
+var b=false;
+console.log(a,b);
+//undefined
+var p;
+console.log(p);
+//null
+// var n=null;
+// console.log(n);
+//arrays...
+// var arr=[1,2,3,4,5,"pratiksha"];
+// console.log(arr[0]);
+//5.operators
+//arithmetic operators
+var a=10;
+var b=20;
+console.log("a +b is",a+b);
+console.log("a +b is",a-b);
+console.log("a +b is",a*b);
+console.log("a +b is",a/b);
+//Assignment operator
+c=b
+c+=2
+console.log(c);
+//Comparison operator ==,<=,>=,<,>
+ var x=10,y=20;
+console.log(x==y);
+//logical operator -operates on booleans
+//logical and
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+//logical or
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+//functions in javascript
+
+function avg(a,b){
+    return (a+b)/2;
+}
+c1=avg(12,16);
+c2=avg(16,10);
+console.log(c1,c2);
+
+//conditionals in javascript
+
+
+
+
